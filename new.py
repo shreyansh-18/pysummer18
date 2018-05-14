5121c275568feaa84a3638843983ae2817775a77
shreyansh-18 <39154270+shreyansh-18@users.noreply.github.com>
2018-05-15 01:23:18 +0530

    Project 1

---
+++ new.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+print "Enter your choice -"
+
+print "1. To google every word of string."
+
+print "2. To search image for every word of string."
+
+print "3. To print URL's of page 1 of google search."
+
+print "4. To print current time and date of system."
+
+print "5. To open default web browser."
+
+print "6. To give network IP's."
+
+print "7. To enter domain and check its owner."
+
+n=raw_input()
+
+if n==1:
+
+
+elif n==2:
+
+
+elif n==3:
+
+
+elif n==4:
+
+
+elif n==5:
+
+
+elif n==6:
+
+
+elif n==7:
