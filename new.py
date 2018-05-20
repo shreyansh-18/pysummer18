07cf993f99008dd742aa931d052786282643d71a
shreyansh-18 <39154270+shreyansh-18@users.noreply.github.com>
2018-05-19 23:58:19 -0400

    Add files via upload

--- new.py
+++ new.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 
 import time
-
+import requests
 import webbrowser
-
+from bs4 import BeautifulSoap
 option='''
 "Enter your choice -"
 "1. To search every word of string."
@@ -24,31 +24,30 @@ if n=='1':
 	#splitting each word by space
 	for i in done_data:
 		webbrowser.open_new_tab('https://www.google.com/search?q='+i)
-	
 
-elif n==2:
-search_data=raw_input("Enter data -")
+elif n=='2':
+	search_data=raw_input("Enter data -")
 	final_data=search_data.strip()
 	#removes space leading and trailing
 	done_data=final_data.split()
 	#splitting each word by space
 	for i in done_data:
-		webbrowser.open_new_tab('https://www.google.com/search?q='+i)
+		webbrowser.open_new_tab('https://www.google.com/search?q='+i+'tbm=isch')
+
+elif n=='3':
+	search=raw_input("Enter data")
+	page=requests.get('https://www.google.com/search?q='+search)
+	soup=BeautifulSoup(page.content,'html.parser')
+	url=[]	
+	url.append(soup.find_all('a'))
+	for i in url:
+		print i
+
+elif n=='4':
+		print time.asctime()
 	
-'''
+elif n=='5':
+		webbrowser.open_new_tab('https://')
 
-
-elif n==3:
-
-
-elif n==4:
-
-
-elif n==5:
-
-
-elif n==6:
-
-
-elif n==7:
-'''
+else
+	print ("Wrong Entry!!")
